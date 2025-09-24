81ea7314add121045665a265cffc4cd0d35522d8
Dominique Broeglin <dominique.broeglin@microsoft.com>
2025-09-24 19:40:36 +0200

    First working App with Apertus + BingSearch

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,22 +1,82 @@
+import logging
 import os
 from textwrap import dedent
-from typing import Any, Callable, Dict
+from typing import Annotated, List, Optional
+
 import chainlit as cl
-from openai import AsyncOpenAI
 import semantic_kernel as sk
-from semantic_kernel.connectors.ai.azure_ai_inference import (
-    AzureAIInferenceChatCompletion,
-    AzureAIInferenceChatPromptExecutionSettings,
+from azure.identity.aio import DefaultAzureCredential
+from openai import AsyncOpenAI
+from semantic_kernel.agents import (
+    AzureAIAgent,
+    AzureAIAgentThread,
+)
+from semantic_kernel.connectors.ai.function_choice_behavior import (
+    FunctionChoiceBehavior,
 )
 from semantic_kernel.connectors.ai.open_ai import (
-    OpenAIPromptExecutionSettings, OpenAIChatCompletion
+    OpenAIChatCompletion,
+    OpenAIPromptExecutionSettings,
 )
 from semantic_kernel.contents import ChatHistory
-from app_tools import register_plugins, router_system_message, execute_tool_call
+from semantic_kernel.functions import kernel_function
 
+logging.basicConfig(
+    format="[%(asctime)s - %(name)s:%(lineno)d - %(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logging.getLogger("kernel").setLevel(logging.DEBUG)
+logging.getLogger("openai").setLevel(logging.DEBUG)
+# Environment variables for model configuration
 
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+MODEL_ENDPOINT = os.environ["MODEL_ENDPOINT"]
+MODEL_ID = os.environ["MODEL_ID"]
+AGENT_ID = os.environ["AGENT_ID"]
+if not MODEL_ENDPOINT:
+    raise RuntimeError("Environment variable MODEL_ENDPOINT must be set.")
+if not MODEL_ID:
+    raise RuntimeError("Environment variable MODEL_ID must be set.")
+if not AGENT_ID:
+    raise RuntimeError("Environment variable AGENT_ID must be set.")
+
+agent_client = AzureAIAgent.create_client(
+    credential=DefaultAzureCredential(),
+    endpoint=os.environ["PROJECT_ENDPOINT"],
+)
+
+class BingPlugin:
+    @kernel_function(
+        name="query_bing",
+        description="Uses a search engine to find information with the query and returns the results.",
+    )
+    async def query_bing(
+        self,
+        query: Annotated[str, "The search query"],
+    ) -> dict:
+        agent_definition = await agent_client.agents.get_agent(agent_id=AGENT_ID)
+        agent = AzureAIAgent(
+            client=agent_client,
+            definition=agent_definition,
+            # plugins=[MenuPlugin()],  # add the sample plugin to the agent
+        )
+        thread: AzureAIAgentThread = AzureAIAgentThread(client=agent_client)
+        response = await agent.get_response(messages=query, thread=thread)
+        text = [item.text for item in response.items if item.content_type == 'text']
+        citations = [{ "url": item.url, "title": item.title } for item in response.items if item.content_type == 'annotation']
+        print("DATA:", response.dict())
+        return {"text": text, "citations": citations}
+
+SYSTEM_MESSAGE = dedent("""
+    You are a helpful assistant that answer user questions politely and concisely.            
+    Use the tools available to you to find information but do NOT answer questions 
+    based on your training data.
+    If you are unsure and the user question is not factual, you can ask the user
+    for clarification.
+    Always use the search tool when you need to find information about current events or
+    anything that is not general knowledge.
+    Never make up answers that are not based on the search results.
+""")
+
 
 # Environment variables for model configuration
 MODEL_ENDPOINT = os.environ["MODEL_ENDPOINT"]
@@ -26,17 +86,13 @@ if not MODEL_ENDPOINT:
 if not MODEL_ID:
     raise RuntimeError("Environment variable MODEL_ID must be set.")
 
-# System message for the assistant agent
-def assistant_system_message() -> str:
-    return (
-        "You are a helpful assistant that answer user questions politely and concisely.\n"
-        "Never call a tool directly, only use the information provided in the message.\n" \
-        "Never hallucinate, if you don't know the answer or the tool does not provide one just say you don't know.\n"
-    )
-
-# Chainlit Chat Starter Setup
+agent_client = AzureAIAgent.create_client(
+    credential=DefaultAzureCredential(),
+    endpoint=os.environ["PROJECT_ENDPOINT"],
+)
+               
 @cl.set_starters
-async def set_starters():
+async def set_starters(user: Optional["cl.User"] = None, project: Optional[str] = None) -> List[cl.Starter]:
     return [
         cl.Starter(
             label="Morning routine ideation",
@@ -65,7 +121,7 @@ async def set_starters():
 async def on_chat_start():
     # Setup Semantic Kernel
     kernel = sk.Kernel()
-
+    
     openai_client = AsyncOpenAI(
         api_key=os.getenv("OPENAI_API_KEY", "vLLM"),  # or None if vLLM ignores it
         base_url=MODEL_ENDPOINT,
@@ -74,104 +130,50 @@ async def on_chat_start():
         async_client=openai_client,
         ai_model_id=MODEL_ID,
     )
-    request_settings = OpenAIPromptExecutionSettings(
+    execution_settings = OpenAIPromptExecutionSettings(
         temperature=0.2,
     )
+    execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto()
 
     kernel.add_service(ai_service)
-    tool_registry = register_plugins(kernel)
-
-    # ROUTER AGENT: Initialize chat hystory and system prompt
-    router_chat_history = ChatHistory()
-    router_chat_history.add_system_message(router_system_message())
+    kernel.add_plugin(
+        BingPlugin(),
+        plugin_name="Bing",
+    )
 
     # ASSISTANT AGENT: Initialize chat history and system prompt
     chat_history = ChatHistory()
-    chat_history.add_system_message(assistant_system_message())
+    chat_history.add_system_message(SYSTEM_MESSAGE)
 
     #cl.user_session.set("sk_filter", sk_filter)
     cl.user_session.set("kernel", kernel)
     cl.user_session.set("ai_service", ai_service)
     cl.user_session.set("chat_history", chat_history)
-    cl.user_session.set("router_chat_history", router_chat_history)
-    cl.user_session.set("tool_registry", tool_registry)
-    cl.user_session.set("request_settings", request_settings)
+    cl.user_session.set("execution_settings", execution_settings)
 
-# Chainlit Message Event
 @cl.on_message
 async def on_message(message: cl.Message):
     kernel = cl.user_session.get("kernel")
     ai_service: OpenAIChatCompletion = cl.user_session.get("ai_service") # type: ignore
     chat_history: ChatHistory = cl.user_session.get("chat_history") # type: ignore #
-    tool_registry: Dict[str, Callable[..., Any]] = cl.user_session.get("tool_registry")  # type: ignore
-    request_settings: OpenAIPromptExecutionSettings = cl.user_session.get("request_settings") # type: ignore
-    router_chat_history: ChatHistory = cl.user_session.get("router_chat_history") # type: ignore
+    execution_settings: OpenAIPromptExecutionSettings = cl.user_session.get("execution_settings") # type: ignore
 
     # Create a Chainlit message for the response stream
-    thinking_answer = cl.Message(content="Thinking...")
-    await thinking_answer.send()
-
-
-    # STEP1: Use the Router agent to determine if we need to call a tool
-    router_chat_history.add_user_message(message.content)
-    router_response = ""
-    async for msg in ai_service.get_streaming_chat_message_content(
-        chat_history=router_chat_history,
-        settings=request_settings,
-        kernel=kernel,
-    ):
-        if msg and msg.content:
-            router_response += msg.content
-    print("Router response:", router_response)
-    answer = cl.Message(content="")
+    answer = cl.Message(content="Thinking...")
     await answer.send()
-    # STEP 2A: if the router decides that no tool is needed (ANSWER_AI), 
-    # we forward the original user message to the assistant agent
-    if "ANSWER_AI" in router_response:
-        print("Router decided to ANSWER_AI")
-        full_response = ""
-        async for msg in ai_service.get_streaming_chat_message_content(
-            chat_history=chat_history,
-            settings=request_settings,
-            kernel=kernel,
-        ):
-            if msg and msg.content:
-                full_response += msg.content
-        print("Assistant response:", full_response)
-        await answer.stream_token(full_response)
-        # Add the full assistant response to history
-        chat_history.add_assistant_message(full_response)
-        return
-    # STEP 2B: If the router decides that a tool is needed (CALL_TOOL), 
-    # we forward the tool call to the assistant agent
-    else:
-        print("Router decided to CALL_TOOL")
-        tool_response = execute_tool_call(router_response, tool_registry)
-        # Updated the answer message with the tool name used
-        answer.author = "Used Tool: " + tool_response[1]
-        await answer.update()
-        # Take the tool call result and send it to the Assistant agent to prepare the final answer
-        response_with_tool = ""
 
-        chat_history.add_user_message(dedent(f"""
-            Information provided by the `{tool_response[1]}` tool:
-            ```
-            {tool_response[0]}
-            ```
-            Answer the user's question by using ONLY the information provided by the tool above DO NOT use multiple tool answers.
-            ```
-            {message.content}
-            ```
-            
-        """))
-        print("--- Chat history:", chat_history)
-        async for msg in ai_service.get_streaming_chat_message_content(
-            chat_history=chat_history,
-            settings=request_settings,
-            kernel=kernel,
-            ):
-                if msg and msg.content:
-                    await thinking_answer.remove()
-                    response_with_tool += msg.content
-                    await answer.stream_token(msg.content)
-        chat_history.add_assistant_message(response_with_tool)
+    chat_history.add_user_message(message.content)
+    await answer.send()
+    msg = await ai_service.get_chat_message_content(
+        chat_history=chat_history,
+        settings=execution_settings,
+        kernel=kernel,
+        )
+    if msg:
+        answer.content = msg.content
+        await answer.update()
+        chat_history.add_assistant_message(msg.content)
+    else:
+        answer.content = "I'm sorry, but I couldn't find a response."
+        await answer.update()
+        chat_history.add_assistant_message("I'm sorry, but I couldn't find a response.")
